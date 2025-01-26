44cd42bcab742023ebe28b9f04f7abfad6b8e3e6
Rishabh Srivastava <104848594+rishabh0777@users.noreply.github.com>
2025-01-26 14:40:26 +0000

    login and signup page ui integration

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import { useNavigate } from 'react-router';
 
 const Login = () => {
+  const navigate = useNavigate();
   return (
-    <div>Login</div>
+    <div className='w-full h-screen flex justify-center items-center'>
+      <div className='w-[50%] h-[90%] bg-zinc-900 shadow-xl shadow-zinc-900/50 rounded-xl flex flex-col items-center gap-4 py-8 text-white'>
+        <h1 className='text-4xl font-poppins tracking-wider'>Login</h1>
+        <p className='text-[1.2vw] tracking-wider font-inter font-thin'>Welcome back! Log in to explore the latest trends in fashion</p>
+        <form 
+        className='w-full flex flex-col items-center gap-4 mt-8'
+        action="">
+          <div className='w-full flex flex-col items-center gap-8'>
+            <input
+              name='email'
+              placeholder='Enter your email'
+              className='outline-none bg-[#333] w-[18vw] py-2 px-2 text-white rounded-full'
+              type="email" />
+
+            <input 
+              name='password'
+              placeholder='Enter your password'
+              className='outline-none bg-[#333] w-[18vw] py-2 px-2 text-white rounded-full'
+              type="password" />
+          </div>
+          
+          <button className='w-[18vw] py-2 px-2 text-white rounded-full bg-indigo-500 mt-6'>Login</button>
+          <div className='flex justify-center items-center gap-2 mt-4'>
+            <p className='text-sm font-thin'>Don't have an account?</p>
+            <a 
+            onClick={()=>navigate('/signup')}
+            className='text-indigo-400 cursor-pointer'>Sign Up</a>
+          </div>
+          <div className='w-[25vw] h-[5vh] flex justify-center items-center gap-2 mt-6'>
+            <span className='border-b-2 border-white w-[7vw]'></span>or continue with<span className='border-b-2 border-white w-[7vw]'></span>
+          </div>
+          <div className='w-[25vw] h-[5vh] gap-4 flex justify-center'>
+            <i className="ri-google-fill text-[1.3vw]"></i>
+            <i className="ri-facebook-circle-fill text-[1.3vw]"></i>
+            <i className="ri-apple-fill text-[1.3vw]"></i>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
-export default Login
+export default Login;
--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -1,22 +1,61 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 
 const Signup = () => {
+  const navigate = useNavigate()
   return (
-    <div className='w-full h-screen flex justify-center items-center bg-gray-100'>
-    <div className='signup w-[39vw] h-[80vh] flex flex-col items-center shadow-lg shadow-indigo-500 rounded-lg py-10'>
-      <h1 className='text-3xl font-poppins mb-8 tracking-wider'>Sign Up</h1>
-      <form>
-        <label for="fname">First name</label><br></br>
-        <input placeholder='Enter your first name' className='px-6 w-[25vw] h-[6vh] shadow shadow-lg shadow-zinc-600 rounded-[4vw] mt-2 mb-3 outline-none'  type="text" id="fname" name="fname" /><br></br>
-        <label for="lname">Last name</label><br></br>
-        <input placeholder='Enter your last name' className='px-6 w-[25vw] h-[6vh] shadow shadow-lg shadow-zinc-600 rounded-[4vw] mt-2 mb-3 outline-none'  type="text" id="lname" name="lname" /><br></br>
-        <label for="email">Email</label><br></br>
-        <input placeholder='Enter your email' className='px-6 w-[25vw] h-[6vh] shadow shadow-lg shadow-zinc-600 rounded-[4vw] mt-2 mb-3 outline-none'  type="text" id="email" name="email" /><br></br>
-        <label for="password">Password</label><br></br>
-        <input placeholder='Enter your password' className='px-6 w-[25vw] h-[6vh] shadow shadow-lg shadow-zinc-600 rounded-[4vw] mt-2 mb-3 outline-none'  type="password" id="password" name="password" /><br></br>
-        <button className='w-[25vw] h-[6vh] shadow shadow-lg rounded-[4vw] mt-2 mb-3 bg-indigo-500 text-white'>Signup</button>
-      </form>
-    </div>
+    <div className='w-full h-screen flex justify-center items-center'>
+      <div className='w-[70%] h-[85%] bg-zinc-900 shadow-xl shadow-zinc-900/50 rounded-xl flex flex-col items-center gap-4 py-8 text-white'>
+        <h1 className='text-4xl font-poppins tracking-wider'>Sign Up</h1>
+        <p className='text-[1.2vw] tracking-wider font-inter font-thin'>Join us and explore the latest trends in fashion</p>
+        <form 
+        className='w-full flex flex-col items-center gap-4 mt-8'
+        action="">
+          <div className='w-full flex justify-center items-center gap-8'>
+          <div className='flex flex-col gap-8'>
+            <input 
+            name='firstName'
+            placeholder='Enter your first name'
+            className='outline-none bg-[#333] w-[18vw] py-2 px-2 text-white rounded-full'
+            type="text" />
+
+            <input 
+            name='lastName'
+            placeholder='Enter your last name'
+            className='outline-none bg-[#333] w-[18vw] py-2 px-2 text-white rounded-full'
+            type="text" />
+          </div>
+          <div className='flex flex-col gap-8'>
+            <input
+            name='email'
+            placeholder='Enter your email'
+            className='outline-none bg-[#333] w-[18vw] py-2 px-2 text-white rounded-full'
+            type="email" />
+
+            <input 
+            name='password'
+            placeholder='Enter your password'
+            className='outline-none bg-[#333] w-[18vw] py-2 px-2 text-white rounded-full'
+            type="password" />
+          </div>
+          
+          </div>
+          <button className='w-1/2 py-2 px-2 text-white rounded-full bg-green-500  mt-10'>Create Account</button>
+          <button 
+          onClick={(e)=>{
+            e.preventDefault()
+            navigate('/login')
+          }  
+            }
+          className='w-1/2 py-2 px-2 text-white rounded-full bg-indigo-500 mt-2'>Login Account</button>
+          <div className='w-[25vw] h-[5vh] flex justify-center items-center gap-2'><span className='border-b-2 border-white w-[7vw]'></span>or continue with<span className='border-b-2 border-white w-[7vw]'></span></div>
+          <div className='w-[25vw] h-[5vh] gap-4 flex justify-center'>
+            <i className="ri-google-fill text-[1.3vw]"></i>
+            <i className="ri-facebook-circle-fill text-[1.3vw]"></i>
+            <i className="ri-apple-fill text-[1.3vw]"></i>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
